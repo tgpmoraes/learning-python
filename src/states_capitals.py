ecbd041031cdb285d6671b57fb23657480c6a30d
tgpmoraes <tgpmoraes@gmail.com>
2017-10-19 06:02:19 -0200

    codigos python iniciais

---
+++ src/states_capitals.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+capitals_dict = {
+    'Acre' : 'Rio Branco',
+    'Alagoas' : 'Maceió',
+    'Amapá' : 'Macapá',
+    'Amazonas' : 'Manaus',
+    'Bahia' : 'Salvador',
+    'Ceará' : 'Fortaleza',
+    'Distrito Federal' : 'Brasília',
+    'Espírito Santo' : 'Vitória',
+    'Goiás' : 'Goiânia',
+    'Maranhão' : 'São Luiz',
+    'Mato Grosso' : 'Cuiabá',
+    'Mato Grosso do Sul' : 'Campo Grande',
+    'Minas Gerais' : 'Belo Horizonte',
+    'Pará' : 'Belém',
+    'Paraíba' : 'João Pessoa',
+    'Paraná' : 'Curitiba',
+    'Pernambuco' : 'Recife',
+    'Piauí' : 'Teresina',
+    'Rio de Janeiro' : 'Rio de Janeiro',
+    'Rio Grande do Norte' : 'Natal',
+    'Rio Grande do Sul' : 'Porto Alegre',
+    'Rondônia' : 'Porto Velho',
+    'Roraima' : 'Boa Vista',
+    'Santa Catarina' : 'Florianópolis',
+    'São Paulo' : 'São Paulo',
+    'Sergipe' : 'Aracajú',
+    'Tocantins' : 'Palmas'
+}
+
+import os, sys
+import random
+
+states = list(capitals_dict.keys())
+for i in [1,2,3,4,5]:
+    state = random.choice(states)
+    capital = capitals_dict[state]
+    capital_guess = raw_input("Qual é a capital do(a) " + state + "? ")
+    
+    if capital_guess == capital:
+        print("Correto! Bom trabalho.")
+    else:
+        print("Incorreto. A capital do(a) " + state + " é " + capital + ".")
+    
+print("Terminado")
